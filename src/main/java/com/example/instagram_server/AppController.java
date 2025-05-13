6e052edb2a4e3319a7d149a9caef2d069306c831
nissi153 <nissisoft21@gmail.com>
2025-05-13 15:53:42 +0900

    docker deploy4

--- src/main/java/com/example/instagram_server/AppController.java
+++ src/main/java/com/example/instagram_server/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String home() {
-        return "Hello, World~ deploy4";
+        return "Hello, World~ docker deploy4";
     }
 }
